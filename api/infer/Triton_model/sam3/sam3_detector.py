b2eb547200f875e4550aa9e9a6a79106596c34fb
NexusAi <nexusai@project.com>
2026-07-21 22:34:01 +0800

    更新sam3

--- api/infer/Triton_model/sam3/sam3_detector.py
+++ api/infer/Triton_model/sam3/sam3_detector.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-@File    : sam3_detector.py.py
+@File    : sam3_detector.py
 @Time    : 2025/12/15 16:44
 @Author  : 陈冲
-@Description : sam3推理
-@Version : 1.0
+@Description : sam3推理 (适配 DART 1008px 高性能版)
+@Version : 1.1
 """
 
 import time
@@ -23,9 +23,11 @@ def sam3(triton_client, service_name, init_data, img, label_rules, box_info, con
     else:
         orig_w, orig_h = img.size
 
+    # 【修改1】尺寸降维到 1008x1008
     img_resized = img.convert('RGB').resize((1008, 1008))
-    # img_data = (np.array(img_resized).astype(np.float32) / 127.5 - 1.0).transpose(2, 0, 1)[None]
-    img_data = np.array(img_resized).astype(np.uint8).transpose(2, 0, 1)[None]
+
+    # 【修改2】转为 float32 类型。ImageNet 归一化已经在服务端的 model.py 中处理，这里直接传 0-255 的 float32 即可
+    img_data = np.array(img_resized).astype(np.float32).transpose(2, 0, 1)[None]
 
     # --- 2. 初始化 time_json ---
     time_json = {
@@ -59,67 +61,59 @@ def sam3(triton_client, service_name, init_data, img, label_rules, box_info, con
     # --- 4. 构造 Prompt ---
     label_list_analyse = list(label_rules.keys()) if isinstance(label_rules, dict) else list(label_rules)
     label_list = [i.replace("sam3_", "") for i in label_list_analyse]
+    TEXT_PROMPTS = np.array([str(x).encode('utf-8') for x in label_list], dtype=object)
 
-    # --- 5. 构造 Triton Inputs 配置 ---
+    # --- 5. 构造 Triton Inputs ---
     img_in_name = INPUT_CFG[0]["name"]
     txt_in_name = INPUT_CFG[1]["name"]
+
+    # 获取服务端配置的数据类型
     img_type = INPUT_CFG[0]["data_type"].split("_")[-1]
     txt_type = INPUT_CFG[1]["data_type"].split("_")[-1]
-    if "STRING" in txt_type:
-        txt_type = "BYTES"
+    if "STRING" in txt_type: txt_type = "BYTES"
 
-    # 将 img_data 转换为模型期望的数据类型
-    _DTYPE_MAP = {"FP32": np.float32, "FP16": np.float16, "UINT8": np.uint8}
-    _target_dtype = _DTYPE_MAP.get(img_type, np.uint8)
-    if _target_dtype in (np.float32, np.float16):
-        # 归一化到 [-1, 1]
-        img_data = (img_data.astype(np.float32) / 127.5 - 1.0).astype(_target_dtype)
-    else:
-        img_data = img_data.astype(_target_dtype)
+    inputs = [
+        grpcclient.InferInput(img_in_name, img_data.shape, img_type),
+        grpcclient.InferInput(txt_in_name, TEXT_PROMPTS.shape, txt_type)
+    ]
+    inputs[0].set_data_from_numpy(img_data)
+    inputs[1].set_data_from_numpy(TEXT_PROMPTS)
 
     outputs = [grpcclient.InferRequestedOutput(obj["name"]) for obj in OUTPUT_CFG]
 
     pretreatment_end = time.time()  # [时间点2] 预处理结束
 
-    # --- 6. 分批推理（sam3 最多支持 8 个 label） ---
-    MAX_LABELS = 8
-    detected_objects = []
-    model_ready = triton_client.is_model_ready(service_name)
-
-    for chunk_start in range(0, len(label_list), MAX_LABELS):
-        chunk_analyse = label_list_analyse[chunk_start:chunk_start + MAX_LABELS]
-        chunk_labels  = label_list[chunk_start:chunk_start + MAX_LABELS]
-
-        TEXT_PROMPTS = np.array([str(x).encode('utf-8') for x in chunk_labels], dtype=object)
-
-        inputs = [
-            grpcclient.InferInput(img_in_name, img_data.shape, img_type),
-            grpcclient.InferInput(txt_in_name, TEXT_PROMPTS.shape, txt_type)
-        ]
-        inputs[0].set_data_from_numpy(img_data)
-        inputs[1].set_data_from_numpy(TEXT_PROMPTS)
-
-        if not model_ready:
-            continue
-
+    # --- 6. 推理 ---
+    output_dict = {}
+    if triton_client.is_model_ready(service_name):
         results = triton_client.infer(service_name, inputs, outputs=outputs)
-        output_dict = {obj["name"]: results.as_numpy(obj["name"]) for obj in OUTPUT_CFG}
+        for obj in OUTPUT_CFG:
+            name = obj["name"]
+            data = results.as_numpy(name)
+            output_dict[name] = data
 
-        if not output_dict:
-            continue
+    postprocessing_start = time.time()  # [时间点3] 后处理开始
 
-        chunk_rules = {k: label_rules[k] for k in chunk_analyse} if isinstance(label_rules, dict) else chunk_analyse
-        detected_objects += postprocess(
-            output_dict,
-            orig_w,
-            orig_h,
-            final_conf_thres,
-            chunk_analyse,
-            label_rules=chunk_rules
-        )
+    # --- 7. 结果判空与后处理 ---
+    detected_objects = []
 
-    postprocessing_start = time.time()
-    postprocessing_end = time.time()
+    if not output_dict:
+        postprocessing_end = time.time()
+        time_json["Pretreatment"] = pretreatment_end - pretreatment_start
+        time_json["post_time"] = postprocessing_start - pretreatment_end
+        time_json["processing_time"] = postprocessing_end - postprocessing_start
+        return detected_objects, time_json
+
+    detected_objects = postprocess(
+        output_dict,
+        orig_w,
+        orig_h,
+        final_conf_thres,
+        label_list_analyse,
+        label_rules=label_rules
+    )
+
+    postprocessing_end = time.time()  # [时间点4] 后处理结束
 
     # --- 8. 时间计算 ---
     time_json["Pretreatment"] = pretreatment_end - pretreatment_start
