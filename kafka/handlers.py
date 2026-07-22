27ecda2bc7e41821f18b946a9dcbe897da32e40c
NexusAi <nexusai@project.com>
2026-07-22 13:32:19 +0800

    修改小模型超时时间

--- kafka/handlers.py
+++ kafka/handlers.py
@@ -20,7 +20,7 @@ from tools.logger_tools import Kafka_Handler_logger as logger
 io_executor = get_io_executor()
 
 VLM_TIMEOUT    = float(os.getenv("VLM_TIMEOUT",    "3"))   # 大模型单次推理超时（秒）
-MODEL_TIMEOUT  = float(os.getenv("MODEL_TIMEOUT",   "5"))   # 小模型单次推理超时（秒）
+MODEL_TIMEOUT  = float(os.getenv("MODEL_TIMEOUT",   "15"))   # 小模型单次推理超时（秒）
 VECTOR_TIMEOUT = float(os.getenv("VECTOR_TIMEOUT",  "3"))   # 向量入库超时（秒）
 
 
